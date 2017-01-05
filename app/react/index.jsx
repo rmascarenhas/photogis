7b605b7984013cf7ee593396d1500d5798739f7a
Renato Mascarenhas <renato@fastmail.jp>
2017-01-05 08:02:39 -0200

    Fix syntax error from mistakenly modified routing.

--- app/react/index.jsx
+++ app/react/index.jsx
@@ -9,14 +9,12 @@ import MainApp from './pages/MainApp';
 // It basically declares the application routes, which are handled
 // by `react-router`. Depending on the URL on the browser, the correct
 // compoenent is rendered.
-const Index = () => ({
-  return (
-    <Router history={browserHistory}>
-      <Route path="/" component={Main} />
-      <Route path="/app" component={MainApp} />
-    </Router>
-  );
-});
+const Index = () => (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main} />
+    <Route path="/app" component={MainApp} />
+  </Router>
+);
 
 ReactDOM.render(
   <Index />,
