73d1d1b3ab26a1ff3e83942663fb6e08e772365d
Renato Mascarenhas <renato@fastmail.jp>
2017-01-09 09:56:55 -0200

    Better docs for PhotoMap.

--- app/react/pages/MainApp/components/PhotoMap.jsx
+++ app/react/pages/MainApp/components/PhotoMap.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
+// PhotoMap
+// This component displays a map (powered by OpenStreetMap) using the leaflet.js
+// dependency. By default it asks for the user's current position, and draws
+// a map centered on that position if available.
+//
+// It also allows users to upload pictures associated with positions on the map.
+// (TODO)
 class PhotoMap extends React.Component {
   constructor() {
     super();
@@ -10,6 +17,8 @@ class PhotoMap extends React.Component {
     };
   }
 
+  // tries to retrieve the user's current position. If provided, update this
+  // component's state with the coordinates, causing the map to be redrawn
   getUserPosition() {
     navigator.geolocation.getCurrentPosition((position) => {
       const coords = position.coords;
